3899675a66d5d08d6036e31a8756dc9899e41dc5
Hieu Dang <xuanhieu1989@gmail.com>
2022-07-15 11:42:29 +0700

    add logic code

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 tfx extension create --manifest-globs vss-extension.json --output-path build
 https://docs.microsoft.com/en-us/azure/devops/extend/develop/add-build-task?view=azure-devops
+npm install azure-devops-node-api --save
--- buildandreleasetask/SetAutomationStatus/index.ts
+++ buildandreleasetask/SetAutomationStatus/index.ts
@@ -1,4 +1,8 @@
 import tl = require('azure-pipelines-task-lib/task');
+import * as azdev from "azure-devops-node-api";
+import * as witAPI from "azure-devops-node-api/WorkItemTrackingApi"
+import * as witInterfaces from 'azure-devops-node-api/interfaces/WorkItemTrackingInterfaces';
+import * as vssInterfaces from 'azure-devops-node-api/interfaces/common/VSSInterfaces'
 
 async function run() {
     try {
@@ -13,6 +17,33 @@ async function run() {
             return;
         }
         console.log('Set automation status for test case ', iTestCaseID);
+        const collectionUri = tl.getVariable('System.TeamFoundationCollectionUri')!;
+        const token = tl.getEndpointAuthorization('SystemVssConnection', true)!.parameters.AccessToken;
+        const project = tl.getVariable('System.TeamProject')!;
+        console.log(`Collection URL: ${collectionUri}`);
+        let authHandler = azdev.getPersonalAccessTokenHandler(token); 
+        let connection = new azdev.WebApi(collectionUri, authHandler); 
+        let wit: witAPI.IWorkItemTrackingApi = await connection.getWorkItemTrackingApi();
+        let wijson: vssInterfaces.JsonPatchDocument = 
+            [
+                { 
+                    "op": "add", 
+                    "path": "/fields/Microsoft.VSTS.TCM.AutomatedTestId",
+                    "value": "1234" 
+                },
+                { 
+                    "op": "add", 
+                    "path": "/fields/Microsoft.VSTS.TCM.AutomatedTestStorage",
+                    "value": "MyDLLName.dll" 
+                },
+                { 
+                    "op": "add", 
+                    "path": "/fields/Microsoft.VSTS.TCM.AutomatedTestName",
+                    "value": "MyDLLName.TestCaseName" 
+                }
+            ];
+        let wi: witInterfaces.WorkItem = await wit.updateWorkItem(null, wijson, iTestCaseID);
+        console.log(`Update success fro test case id : ${iTestCaseID}`); 
     }
     catch (err) {
         let errorMessage = "Failed to do something exceptional";
