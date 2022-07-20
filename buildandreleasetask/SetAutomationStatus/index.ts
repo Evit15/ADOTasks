f9fa178cc0b315e8159be12456eecd2dc1283699
Hieu Dang <xuanhieu1989@gmail.com>
2022-07-20 15:04:50 +0700

    add flow code

--- buildandreleasetask/SetAutomationStatus/index.ts
+++ buildandreleasetask/SetAutomationStatus/index.ts
@@ -3,26 +3,56 @@ import * as azdev from "azure-devops-node-api";
 import * as witAPI from "azure-devops-node-api/WorkItemTrackingApi"
 import * as witInterfaces from 'azure-devops-node-api/interfaces/WorkItemTrackingInterfaces';
 import * as vssInterfaces from 'azure-devops-node-api/interfaces/common/VSSInterfaces'
+import * as te from "azure-devops-node-api/TestApi";
+import * as ti from "azure-devops-node-api/interfaces/TestInterfaces";
+
+async function getListTestCaseId(connection: azdev.WebApi, project: string){
+    let testIds: number[] = [];
+    const sTestPlanID = tl.getInput('testPlan', true)!;
+    const sTestSuiteID = tl.getInput('testSuite', true)!;
+    const iTestPlanID: number = parseInt(sTestPlanID);
+    const iTestSuiteID: number = parseInt(sTestSuiteID);
+    console.log(`Test plan id: ${iTestPlanID}`);
+    console.log(`Test suite id: ${iTestSuiteID}`);
+    let test: te.ITestApi = await connection.getTestApi();
+    let tps: ti.TestPoint[] = await test.getPoints(project, iTestPlanID, iTestSuiteID);
+    tps.forEach(tp => {
+        console.log(`Test point id: ${tp.id} with test case id: ${tp.testCase.id}`);
+        if(tp.testCase.id != undefined){
+            let iTestCaseID: number = parseInt(tp.testCase.id);
+            testIds.push(iTestCaseID);
+        }
+    });
+    return testIds;
+}
 
 async function run() {
     try {
-        const inputString: string | undefined = tl.getInput('testcaseid', true);
-        if (inputString == undefined || inputString == '') {
-            tl.setResult(tl.TaskResult.Failed, 'Please input test case id');
-            return;
-        }
-        const iTestCaseID: number = parseInt(inputString);
-        if (iTestCaseID == 0 || iTestCaseID == NaN){
-            tl.setResult(tl.TaskResult.Failed, 'Please input test case id is number');
-            return;
-        }
-        console.log('Set automation status for test case ', iTestCaseID);
+        const setSelector: string = tl.getInput("setSelector", true)!;
+        let testIds: number[] = [];
         const collectionUri = tl.getVariable('System.TeamFoundationCollectionUri')!;
         const token = tl.getEndpointAuthorization('SystemVssConnection', true)!.parameters.AccessToken;
         const project = tl.getVariable('System.TeamProject')!;
         console.log(`Collection URL: ${collectionUri}`);
         let authHandler = azdev.getPersonalAccessTokenHandler(token); 
         let connection = new azdev.WebApi(collectionUri, authHandler); 
+        if(setSelector === 'testCase'){
+            const inputString: string | undefined = tl.getInput('testcaseid', true);
+            if (inputString == undefined || inputString == '') {
+                tl.setResult(tl.TaskResult.Failed, 'Please input test case id');
+                return;
+            }
+            const iTestCaseID: number = parseInt(inputString);
+            if (iTestCaseID == 0 || iTestCaseID == NaN){
+                tl.setResult(tl.TaskResult.Failed, 'Please input test case id is number');
+                return;
+            }
+            console.log('Set automation status for test case ', iTestCaseID);
+            testIds.push(iTestCaseID);
+        }else{
+            testIds = await getListTestCaseId(connection, project);
+        }
+        
         let wit: witAPI.IWorkItemTrackingApi = await connection.getWorkItemTrackingApi();
         let wijson: vssInterfaces.JsonPatchDocument = 
             [
@@ -42,8 +72,11 @@ async function run() {
                     "value": "MyDLLName.TestCaseName" 
                 }
             ];
-        let wi: witInterfaces.WorkItem = await wit.updateWorkItem(null, wijson, iTestCaseID);
-        console.log(`Update success fro test case id : ${iTestCaseID}`); 
+        testIds.forEach( async tc => {
+            let wi: witInterfaces.WorkItem = await wit.updateWorkItem(null, wijson, tc);
+            console.log(`Update success fro test case id : ${tc}`); 
+        });
+        console.log(`All test cases are updated successfully!`); 
     }
     catch (err) {
         let errorMessage = "Failed to do something exceptional";
